ae9c2debc0b20dc56b9e3e078d2effa37fe26af9
Mihai Chilom <mihai.chilom@yhaoo.com>
2024-09-18 17:14:05 +0300

    display country home page wip

---
+++ components/card/CountryFlagAndName.tsx
@@ -0,0 +1,24 @@
+import { findCountryByCode } from "@/utils/countries";
+import React from "react";
+import Image from "next/image";
+
+function CountryFlagAndName({ countryCode }: { countryCode: string }) {
+  const validCountry = findCountryByCode(countryCode)!;
+  const countryName =
+    validCountry.name.length > 20
+      ? `${validCountry.name.substring(0, 20)}...`
+      : validCountry.name;
+  return (
+    <span className="flex justify-between items-center gap-2 text-sm">
+      <Image
+        src={validCountry.flagUrl}
+        alt={`${validCountry.name} flag`}
+        width={16}
+        height={12}
+      />
+      {countryName}
+    </span>
+  );
+}
+
+export default CountryFlagAndName;
