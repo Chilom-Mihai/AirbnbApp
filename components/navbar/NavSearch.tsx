c31a9f5edda370a5bfd12db6ce5a16f3f00b027a
Mihai Chilom <mihai.chilom@yhaoo.com>
2024-09-20 10:54:39 +0300

    search fixed

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import React from "react";
-import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
-import { useState, useEffect } from "react";
+import { Input } from "../ui/input";
 
 function NavSearch() {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
 
   const [search, setSearch] = useState(
@@ -22,7 +20,7 @@ function NavSearch() {
     } else {
       params.delete("search");
     }
-    replace(`${pathname}?${params.toString()}`);
+    replace(`/?${params.toString()}`);
   }, 500);
 
   useEffect(() => {
